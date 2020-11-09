6fb63d0b08804bbeadc9d92c75c3d13bc0c287d2
amirrezamds <43412798+amirrezamds@users.noreply.github.com>
2020-11-08 22:40:59 -0500

    Add files via upload

---
+++ like-button.js
@@ -0,0 +1,82 @@
+// store the main Firebase URL
+var firebaseURL = 'https://test-5ef88.firebaseio.com/tweets/';
+    var userId = user.uid; ;
+console.log(userId);
+// update the likeCounts shown in a <span> beside each blogpost
+var postDivs = document.querySelectorAll('.post');
+
+for (var i = 0; i < postDivs.length; i++) {
+
+    var postID = postDivs[i].id;
+
+    var numLikes = getLikeCount(postID);
+
+}
+
+// this function grabs the likeCount for a particular post from the Firebase
+function getLikeCount(postID) {
+    console.log('running getLikeCount for post ID:', postID);
+    
+    var thisPostRef = new Firebase(firebaseURL + userId + '/like-count/');
+    
+    thisPostRef.once('value', function(snapshot) {
+        
+        console.log( postID + ' value:', snapshot.val() );
+        
+        if ( snapshot.val() ) {
+            
+            console.log( postID + ' contains:', snapshot.val() );
+
+            document.querySelector('#' + postID + ' .like-count').innerHTML = snapshot.val() + ' likes';
+            
+        } else {
+            
+            console.log( postID + '- no data in Firebase' );
+            
+            return 0;
+        
+        }
+    
+    });
+    
+}
+
+function likePost(id) {
+
+    console.log('running likePost() for post ID:', id);
+    
+    var postRef = new Firebase(firebaseURL + id);
+    
+    // get current number of likes here, so we can increment if any exist
+    postRef.child('like-count').once('value', function(snapshot){
+        
+        console.log( 'snapshot.val():', snapshot.val() );
+        
+        var currentLikes = snapshot.val() ? snapshot.val() : 0;
+        
+        console.log( 'currentLikes:', currentLikes );
+    document.getElementById('mypr').innerHTML="Number of Likes: " + currentLikes;
+        postRef.update({
+            
+            'postID': id,
+            'like-count': currentLikes + 1
+            
+            }, function(error) {
+                
+              if (error) {
+                  
+                console.log('Data could not be saved:' + error);
+              
+              } else {
+              
+                console.log('Data saved successfully');
+              
+              }
+            
+            });
+            
+        getLikeCount(id);
+    
+    });
+    
+}
