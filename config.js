1a5d6a573136edc4e259d36c8c39be9d8c3ad283
amirrezamds <43412798+amirrezamds@users.noreply.github.com>
2020-11-28 21:21:27 -0500

    Update config.js

--- config.js
+++ config.js
@@ -43,12 +43,17 @@
       {
         id: 112292,
         login: "amirmds",
-        password: "amirreza82mds"
+        password: "12345678"
       },
         {
         id: 112471,
         login:"jamesliao",
         password:"jamesisdumb"
+        },
+        {
+        id: 2830771,
+        login:"SoltoonDara",
+        password:"12345678"
         }
     ];
 
